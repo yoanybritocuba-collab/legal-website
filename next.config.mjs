491a77f9626d4b7ad201ccbb4363d5ff9d60fd6e
yoany-brito <yoanybritocuba@gmail.com>
2026-03-05 15:55:31 +0100

    Descripción del cambio

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',  // Esto genera archivos estáticos
+  output: 'export',
   images: {
     unoptimized: true,
   },
   trailingSlash: true,
+  // Desactivar optimización de fuentes para evitar advertencias
+  experimental: {
+    optimizeFonts: false,
+  },
 };
 
 export default nextConfig;
